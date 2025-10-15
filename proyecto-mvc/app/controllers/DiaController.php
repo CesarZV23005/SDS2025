27e81553a819e182ea213719bdaa84cdf3cfe21d
CesarZV23005 <144456953+CesarZV23005@users.noreply.github.com>
2025-10-14 22:54:09 -0600

    ft: enrutamiento de los dias restantes

--- proyecto-mvc/app/controllers/DiaController.php
+++ proyecto-mvc/app/controllers/DiaController.php
@@ -5,6 +5,18 @@ class DiaController {
     public function dia1(){
         return $this->view('dia1', ['title' => 'dia1']);
     }
+    public function dia2(){
+        return $this->view('dia1', ['title' => 'dia2']);
+    }
+    public function dia3(){
+        return $this->view('dia3', ['title' => 'dia3']);
+    }
+    public function dia4(){
+        return $this->view('dia4', ['title' => 'dia4']);
+    }
+    public function dia5(){
+        return $this->view('dia5', ['title' => 'dia5']);
+    }
     private function view($vista, $data = []){
         extract($data);
         $rutaVista = __DIR__ . "/../views/$vista.php";
