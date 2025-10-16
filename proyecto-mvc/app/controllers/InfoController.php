4431149c7fabf317722f448a71b8e03888f99fea
CesarZV23005 <zv23005@ues.edu.sv>
2025-10-15 22:25:38 -0600

    ft: implementacion de InfoController

--- proyecto-mvc/app/controllers/InfoController.php
+++ proyecto-mvc/app/controllers/InfoController.php
@@ -2,9 +2,17 @@
 namespace app\controllers;
 
 class InfoController {
-    public function index() {
-        ob_start();
-        include __DIR__ . "/../views/info.php";
-        return ob_get_clean();
+    public function info(){
+        return $this->view('info', ['title' => 'Bienvenido']);
+    }
+    private function view($vista, $data = []){
+        extract($data);
+        $rutaVista = __DIR__ . "/../views/$vista.php";
+        if(file_exists($rutaVista)){
+            ob_start();
+            include $rutaVista;
+            return ob_get_clean();
+        }
+        return "Vista no encontrada: $vista";
     }
 }
